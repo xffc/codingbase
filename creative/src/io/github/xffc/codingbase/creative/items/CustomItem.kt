f2d0dc3ec1d801551f2e75eb945af3ac8789662c
xffc <xffc.sk@gmail.com>
2026-06-22 21:08:54 +0300

    тест

--- creative/src/io/github/xffc/codingbase/creative/items/CustomItem.kt
+++ creative/src/io/github/xffc/codingbase/creative/items/CustomItem.kt
@@ -3,7 +3,7 @@ package io.github.xffc.codingbase.creative.items
 import org.bukkit.event.inventory.InventoryClickEvent
 
 sealed class CustomItem {
-
+// s
 
     abstract fun onClick(event: InventoryClickEvent)
 }

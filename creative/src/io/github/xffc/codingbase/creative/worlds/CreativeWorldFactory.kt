762eb0be65a9ef422874df0af88370e01a888508
xffc <xffc.sk@gmail.com>
2026-06-23 05:08:44 +0300

    геймрулы, барьер

--- creative/src/io/github/xffc/codingbase/creative/worlds/CreativeWorldFactory.kt
+++ creative/src/io/github/xffc/codingbase/creative/worlds/CreativeWorldFactory.kt
@@ -4,6 +4,7 @@ import io.github.xffc.codingbase.creative.CreativePlugin.WORLDS_PREFIX
 import io.github.xffc.codingbase.creative.data.CreativeWorldInfo
 import io.github.xffc.codingbase.creative.worlds.generator.SimpleBiomeProvider
 import io.github.xffc.codingbase.creative.worlds.generator.WorldGenerator
+import org.bukkit.GameRules
 import org.bukkit.Location
 import org.bukkit.World
 import org.bukkit.WorldCreator
@@ -20,9 +21,33 @@ object CreativeWorldFactory {
 
     fun create(info: CreativeWorldInfo): CreativeWorld {
         val instance = createInstance(info)
+
         val center = info.size.toDouble() * 16 / 2
+
         instance.spawnLocation = Location(instance, center, WorldGenerator.SPAWN_HEIGHT.toDouble(), center)
-        // todo: геймрулы, worldborder
+
+        instance.worldBorder.setCenter(center, center)
+        instance.worldBorder.size = info.size.toDouble() * 16
+
+        instance.setGameRule(GameRules.RAIDS, false)
+        instance.setGameRule(GameRules.SPAWN_MOBS, false)
+        instance.setGameRule(GameRules.LOCATOR_BAR, false)
+        instance.setGameRule(GameRules.ADVANCE_TIME, false)
+        instance.setGameRule(GameRules.SPAWN_WARDENS, false)
+        instance.setGameRule(GameRules.SPAWN_PATROLS, false)
+        instance.setGameRule(GameRules.SPAWN_PHANTOMS, false)
+        instance.setGameRule(GameRules.SPAWN_MONSTERS, false)
+        instance.setGameRule(GameRules.ADVANCE_WEATHER, false)
+        instance.setGameRule(GameRules.IMMEDIATE_RESPAWN, true)
+        instance.setGameRule(GameRules.COMMAND_BLOCKS_WORK, false)
+        instance.setGameRule(GameRules.SHOW_DEATH_MESSAGES, false)
+        instance.setGameRule(GameRules.GLOBAL_SOUND_EVENTS, false)
+        instance.setGameRule(GameRules.SPAWN_WANDERING_TRADERS, false)
+        instance.setGameRule(GameRules.SHOW_ADVANCEMENT_MESSAGES, false)
+        instance.setGameRule(GameRules.SPECTATORS_GENERATE_CHUNKS, false)
+        instance.setGameRule(GameRules.ALLOW_ENTERING_NETHER_USING_PORTALS, false)
+        instance.setGameRule(GameRules.RESPAWN_RADIUS, 0)
+
         return register(instance, info)
     }
 
